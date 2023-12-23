a61d7c7b38e0c9a5b79adab29fd69188cac4b87f
MMitsuk <151979175+MMitsuk@users.noreply.github.com>
2023-12-23 10:58:44 +0700

    Update zadanie 2.cpp

--- zadanie 2.cpp
+++ zadanie 2.cpp
@@ -4,8 +4,6 @@
 #define M_PI 3.14159265358979323846
 using namespace std;
 
-void function_output(float radian);
-void function_output(double radian);
 void function_output(long double radian);
 long double convertation(long double radian);
 
@@ -15,11 +13,13 @@ int main()
     float radianf;
     double radiand;
     long double radian;
+    const long double a = 90.0, p = 180.0, c = 270.0, d = 360.0;
     int i;
     while (1)
     {
         cout << "В каких значениях вы хотите ввести значение?\n1 - Радиан\n2 - Градусы\n";
         cin >> i;
+        start:
         cout << "Введите значение: ";
         switch (i)
         {
@@ -33,11 +33,40 @@ int main()
             break;
         case 2:
             cin >> radian;
+            if (radian > 360.0) { cout << "Введите значение меньше 360 "; goto start; }
+            if (radian == a)
+            {
+                cout << setprecision(7) << "Синус: " << 1 << endl
+                    << "Косинус: " << 0 << endl
+                    << "Тангенс: " <<  "inf" << endl
+                    << "Котангенс: " << 0 <<endl;
+                break;
+            }
+            else if (radian == c)
+            {
+                cout << setprecision(7) << "Синус: " << 0 << endl
+                    << "Косинус: " << -1 << endl
+                    << "Тангенс: " << 0 << endl
+                    << "Котангенс: " << "inf" << endl;
+                break;
+            }
+            else if (radian == p)
+            {
+                cout << setprecision(7) << "Синус: " << 0 << endl
+                    << "Косинус: " << -1 << endl
+                    << "Тангенс: " << 0 << endl
+                    << "Котангенс: " << "inf" << endl;
+                break;
+            }
+            else if (radian == d)
+            {
+                cout << setprecision(7) << "Синус: " << 0 << endl
+                    << "Косинус: " << 1 << endl
+                    << "Тангенс: " << 0 << endl
+                    << "Котангенс: " << "inf" << endl;
+                break;
+            }
             radian = convertation(radian);
-            radiand = radian;
-            radianf = radiand;
-            function_output(radianf);
-            function_output(radiand);
             function_output(radian);
             break;
         }
@@ -46,27 +75,18 @@ int main()
 long double convertation(long double grad)
 {
     long double radian;
-    radian = (grad * M_PI)/180;
+    radian = (grad * M_PI) / 180.0;
     return radian;
 }
-void function_output(float radian)
+
+void function_output(long double radian)
 {
-    cout << setprecision(7) << "Синус: " << sin(radian) << endl 
-        << "Косинус: " << cos(radian) << endl 
-        <<  "Тангенс: " << tan(radian) << endl 
-        << "Котангенс: " << 1.0 / tan(radian) << endl << endl;
-}
-void function_output(double radian)
-{
-    cout << setprecision(26) << "Синус: " << sin(radian) << endl
-        << "Косинус: " << cos(radian) << endl
-        << "Тангенс: " << tan(radian) << endl
-        << "Котангенс: " << 1.0 / tan(radian) << endl << endl;
-}
-void function_output(long double radian) 
-{
-    cout << setprecision(35) << "Синус: " << sin(radian) << endl
-        << "Косинус: " << cos(radian) << endl
-        << "Тангенс: " << tan(radian) << endl
-        << "Котангенс: " << 1.0 / tan(radian) << endl << endl;
+    float radianf;
+    double radiand;
+    radiand = radian;
+    radianf = radiand;
+    cout << setprecision(100) << "Синус: " << endl << "f: " << sin(radianf) << endl << "d: " << sin(radiand) << endl << "ld : " << sin(radian) << endl
+        << "Косинус: " << endl << "f: " << cos(radianf) << endl << "d: " << cos(radiand) << endl << "ld: " << cos(radian) << endl
+        << "Тангенс: " << endl << "f: " << tan(radianf) << endl << "d: " << tan(radiand) << endl << "ld: " << tan(radian) << endl
+        << "Котангенс: " << endl << "f: " << 1.0 / tan(radianf) << endl << "d: " << 1.0 / tan(radiand) << endl << "ld: " << 1.0 / tan(radian) << endl;
 }
